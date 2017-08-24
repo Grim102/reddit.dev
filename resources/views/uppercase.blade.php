ed93870b4d9cdf2958421e935deb492d8c595220
Alex Endacott <endacott.alex@gmail.com>
2017-08-24 13:12:17 -0500

    refactor styling

--- resources/views/uppercase.blade.php
+++ resources/views/uppercase.blade.php
@@ -5,8 +5,8 @@
 @endsection
 
 @section('content')
-<div class="jumbotron">
+<div class="jumbotron text-center">
 	<h1>{{ $uppercase }}</h1>
+<h2><a href="{{ action('HomeController@lowercase', array($uppercase))  }}">Lowercase</a></h2>
 </div>
-<a href="{{ action('HomeController@lowercase', array($uppercase))  }}">Lowercase</a>
 @endsection
