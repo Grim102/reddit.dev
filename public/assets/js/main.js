5b7332323ca2fb80d90b62b04df2cd607754ef5d
Alex Endacott <endacott.alex@gmail.com>
2017-09-04 08:08:44 -0500

    update styling

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,7 +4,12 @@ $( document ).ready(function() {
 		selector: ".text-form",
 	});
 
-	$('#blog-post > p').css('line-height', '40px');
+	$('#blog-post > p').css({
+		'line-height': '40px',
+		'margin': '0px 0px 20px',
+		'paddig': '0px',
+		'letter-spacing': '0px'
+		});
 
 	$('.pagination').addClass('pagination-lg');
 
